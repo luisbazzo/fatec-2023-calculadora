28bd93eb1d45b4ceb3eac728395213d73c0e2a06
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-02-28 19:16:13 -0300

    Projeto pre-finalizado

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ function multiplicar(){
     num1 = parseFloat(document.getElementById("num1").value);
     num2 = parseFloat(document.getElementById("num2").value);
 
-    resp = num1 + num2;
+    resp = num1 * num2;
 
     document.getElementById("resp").value = resp;
 
@@ -44,10 +44,43 @@ function dividir(){
     num1 = parseFloat(document.getElementById("num1").value);
     num2 = parseFloat(document.getElementById("num2").value);
 
-    resp = num1 + num2;
+    resp = num1 / num2;
 
     document.getElementById("resp").value = resp;
 
+}
+
+function mostrarOperacao(){
+
+    document.getElementById("listaOp").style.display = "block";
+    document.getElementById("btnNada").style.borderRadius = "0";
+    document.getElementById("btnNada").style.borderBottom = "2px solid black";
+    document.getElementById("btnNada").style.display = "inline-block";
+    document.getElementById("btnSomar").style.display = "none";
+    document.getElementById("btnSubtrair").style.display = "none";
+    document.getElementById("btnMultiplicar").style.display = "none";
+
+}
+
+function selecionarOperacao(op){
+
+    document.getElementById("btnNada").style.display = "none";
+
+    if(op == "Somar"){
+        document.getElementById("btnSomar").style.display = "inline-block";
+    }
+    else if(op == "Subtrair"){
+        document.getElementById("btnSubtrair").style.display = "inline-block";
+    }
+    else if(op == "Multiplicar"){
+        document.getElementById("btnMultiplicar").style.display = "inline-block";
+    }
+    else{
+        document.getElementById("btnDividir").style.display = "inline-block";
+    }
+
+    document.getElementById("listaOp").style.display = "none";
+    
 }
 
 function limpar(){
@@ -55,4 +88,11 @@ function limpar(){
     document.getElementById("num1").value = "";
     document.getElementById("num2").value = "";
     document.getElementById("resp").value = "";
+    document.getElementById("listaOp").style.display = "none";
+    document.getElementById("btnNada").style.display = "inline-block";
+    document.getElementById("btnSomar").style.display = "none";
+    document.getElementById("btnSubtrair").style.display = "none";
+    document.getElementById("btnMultiplicar").style.display = "none";
+    document.getElementById("btnDividir").style.display = "none";
+    document.getElementById("btnNada").style.borderRadius = "40px";
 }
